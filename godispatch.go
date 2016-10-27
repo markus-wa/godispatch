a957c04556a8c87a0797b9139e0b7ecae03ad0d5
Markus Walther <m.walther97@gmail.com>
2016-10-27 22:23:13 +0200

    Hid handlerLock from callees

--- godispatch.go
+++ godispatch.go
@@ -17,7 +17,7 @@ var syncToken *struct{} = &struct{}{}
 
 // Dispatcher is used to register Handlers and dispatch objects
 type Dispatcher struct {
-	sync.RWMutex
+	handlerLock    sync.RWMutex
 	syncLock       sync.Mutex
 	syncWg         sync.WaitGroup
 	handlers       map[reflect.Type][]Handler
@@ -26,16 +26,16 @@ type Dispatcher struct {
 
 // Dispatch dispatches an object to all it's handlers in the order in which the handlers were registered
 func (d *Dispatcher) Dispatch(object interface{}) {
-	d.RLock()
-	defer d.RUnlock()
+	d.handlerLock.RLock()
+	defer d.handlerLock.RUnlock()
 
 	t := reflect.TypeOf(object)
 
 	if d.cachedHandlers[t] == nil {
 		// We'll need a write lock inside
-		d.RUnlock()
+		d.handlerLock.RUnlock()
 		d.initCache(t)
-		d.RLock()
+		d.handlerLock.RLock()
 	}
 
 	for _, h := range d.cachedHandlers[t] {
@@ -48,8 +48,8 @@ func (d *Dispatcher) Dispatch(object interface{}) {
 // We cache the handlers so we don't have to check the type of each handler group for every object of the same type
 // Performance gained >15%, depending on the amount of handlers
 func (d *Dispatcher) initCache(handlerType reflect.Type) {
-	d.Lock()
-	defer d.Unlock()
+	d.handlerLock.Lock()
+	defer d.handlerLock.Unlock()
 
 	// Read from nil map is allowed, so we initialize it only now if it's nil
 	if d.cachedHandlers == nil {
@@ -65,7 +65,7 @@ func (d *Dispatcher) initCache(handlerType reflect.Type) {
 	}
 }
 
-func (d *Dispatcher) dispatchQueue(q chan interface{}) {
+func (d *Dispatcher) dispatchQueue(q <-chan interface{}) {
 	for e := range q {
 		if e == syncToken {
 			d.syncWg.Done()
@@ -77,20 +77,20 @@ func (d *Dispatcher) dispatchQueue(q chan interface{}) {
 
 // AddQueue adds a channel as 'object-queue'
 // All objects sent to the passed channel will be dispatched in a seperate go routine
-func (d *Dispatcher) AddQueue(q chan interface{}) {
+func (d *Dispatcher) AddQueue(q <-chan interface{}) {
 	go d.dispatchQueue(q)
 }
 
 // SyncQueues syncs the channels dispatch routines to the current go routine
 // This ensures all objects received in the passed channel up to this point will be handled before continuing
-func (d *Dispatcher) SyncQueues(queues ...chan interface{}) {
+func (d *Dispatcher) SyncQueues(queues ...chan<- interface{}) {
 	for _, q := range queues {
 		d.syncQueue(q)
 	}
 }
 
 // See SyncQueues
-func (d *Dispatcher) syncQueue(queue chan interface{}) {
+func (d *Dispatcher) syncQueue(queue chan<- interface{}) {
 	// We can not check the channel length as that does not tell us whether the last object has been fully dispatched
 	d.syncLock.Lock()
 	defer d.syncLock.Unlock()
@@ -103,8 +103,8 @@ func (d *Dispatcher) syncQueue(queue chan interface{}) {
 // RegisterHandler registers an object handler for a type of objects
 // If an object's type is assignable to objectType (reflect.Type.assignableTo), the handler will receive the object
 func (d *Dispatcher) RegisterHandler(objectType reflect.Type, handler Handler) {
-	d.Lock()
-	defer d.Unlock()
+	d.handlerLock.Lock()
+	defer d.handlerLock.Unlock()
 
 	if d.handlers == nil {
 		d.handlers = make(map[reflect.Type][]Handler)
