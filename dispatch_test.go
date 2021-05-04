f96a4c24d79ca93026d9386b88ba28e88355fcbc
Markus Walther <m.walther97@gmail.com>
2021-05-04 18:52:07 +0100

    tests: fix linting issues

--- dispatch_test.go
+++ dispatch_test.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	dp "github.com/markus-wa/godispatch"
 )
 
@@ -38,158 +40,163 @@ type result struct {
 	bPtrVal *B
 }
 
-func (r result) equal(r2 result) bool {
-	b := true
-	b = b && r.strVal == r2.strVal
-	b = b && r.intVal == r2.intVal
-	b = b && r.f32Val == r2.f32Val
-	b = b && r.f64Val == r2.f64Val
-	if r.abVal != nil {
-		if r2.abVal == nil {
-			b = false
-		} else {
-			b = b && r.abVal.foo() == r2.abVal.foo()
-		}
-	}
-	b = b && r.aVal.val == r2.aVal.val
-	if r.bPtrVal != nil {
-		b = b && r.bPtrVal == r2.bPtrVal
-	}
-	return b
-}
+func registerHandlers(t *testing.T, d *dp.Dispatcher) *result {
+	t.Helper()
 
-func assertResult(t *testing.T, res *result, exp result) {
-	if !res.equal(exp) {
-		t.Error("expected ", exp, "got", res)
-	}
-}
-
-func registerHandlers(d *dp.Dispatcher) *result {
 	res := &result{}
 
 	// string handler
 	d.RegisterHandler(func(s string) {
 		res.strVal = s
-		fmt.Println("Handled string", res.strVal)
+		t.Log("Handled string", res.strVal)
 	})
 	// int handler
 	d.RegisterHandler(func(i int) {
 		res.intVal = i
-		fmt.Println("Handled int", res.intVal)
+		t.Log("Handled int", res.intVal)
 	})
 	// float32 handler
 	d.RegisterHandler(func(f float32) {
 		res.f32Val = f
-		fmt.Println("Handled float32", res.f32Val)
+		t.Log("Handled float32", res.f32Val)
 	})
 	// float64 handler
 	d.RegisterHandler(func(f float64) {
 		res.f64Val = f
-		fmt.Println("Handled float64", res.f64Val)
+		t.Log("Handled float64", res.f64Val)
 	})
 	// interface handler
 	d.RegisterHandler(func(ab AB) {
 		res.abVal = ab
-		fmt.Println("Handled AB", res.abVal)
+		t.Log("Handled AB", res.abVal)
 	})
 	// struct handler
 	d.RegisterHandler(func(a A) {
 		res.aVal = a
-		fmt.Println("Handled A", res.aVal)
+		t.Log("Handled A", res.aVal)
 	})
 	// pointer handler (no reflect.Type.Elem() call)
 	d.RegisterHandler(func(bPtr *B) {
 		res.bPtrVal = bPtr
-		fmt.Println("Handled *B", res.bPtrVal)
+		t.Log("Handled *B", res.bPtrVal)
 	})
+
 	return res
 }
 
 func TestString(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := "test"
 	d.Dispatch(val)
-	assertResult(t, res, result{strVal: val})
+
+	assert.Equal(t, result{strVal: val}, *res)
 }
 
 func TestInt(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := 13
 	d.Dispatch(val)
-	assertResult(t, res, result{intVal: val})
+
+	assert.Equal(t, result{intVal: val}, *res)
 }
 
 func TestFloat32(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := float32(1.23)
 	d.Dispatch(val)
-	assertResult(t, res, result{f32Val: val})
+
+	assert.Equal(t, result{f32Val: val}, *res)
 }
 
 // Should only be handled by float64 handler, not float32
 func TestFloat64(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := 9.87
 	d.Dispatch(val)
-	assertResult(t, res, result{f64Val: val})
+
+	assert.Equal(t, result{f64Val: val}, *res)
 }
 
 func TestStructA(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := A{val: 17}
 	d.Dispatch(val)
-	assertResult(t, res, result{abVal: val, aVal: val})
+
+	assert.Equal(t, result{abVal: val, aVal: val}, *res)
 }
 
 // Tests if the handler cache is cleared after registering an interface handler
 func TestStructB(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 
 	val := B{val: "bVal"}
-	// Make Dispatcher initalize the cache
+	// Make Dispatcher initialize the cache
 	d.Dispatch(val)
 
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	d.Dispatch(val)
-	assertResult(t, res, result{abVal: val})
+
+	assert.Equal(t, result{abVal: val}, *res)
 }
 
 // Should work with pointers just as well
 func TestStructPointer(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	val := &B{val: "bPtrVal"}
 	d.Dispatch(val)
-	assertResult(t, res, result{abVal: val, bPtrVal: val})
+
+	assert.Equal(t, result{abVal: val, bPtrVal: val}, *res)
 }
 
 // Tests queue functionality
 func TestQueues(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
-	res := registerHandlers(&d)
+	res := registerHandlers(t, &d)
 
 	q := make(chan interface{})
 
 	d.AddQueues(q)
+
 	strVal := "txt"
 	q <- strVal
-	exp := result{strVal: strVal}
-	d.SyncAllQueues()
-	assertResult(t, res, exp)
 
-	d.RemoveQueues(q)
+	d.SyncAllQueues()
+
+	exp := result{strVal: strVal}
+	assert.Equal(t, exp, *res)
+
+	err := d.RemoveQueues(q)
+	assert.NoError(t, err)
+
 	select {
 	case q <- 10:
 		// Nobody should be receiving on this channel after removal
@@ -199,27 +206,33 @@ func TestQueues(t *testing.T) {
 	}
 
 	d.AddQueues(q)
+
 	f32Val := float32(0.5)
 	q <- f32Val
-	exp.f32Val = f32Val
+
 	d.SyncAllQueues()
-	assertResult(t, res, exp)
+
+	exp.f32Val = f32Val
+	assert.Equal(t, exp, *res)
 }
 
 // Tests how handlers behave if added dynamically by other handlers
 func TestAddHandlerInHandler(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 	h1 := 0
 	h2 := 0
 	h3 := 0
+
 	d.RegisterHandler(func(i int) {
-		fmt.Println("Handled", i, "in h1")
+		t.Log("Handled", i, "in h1")
 		if h1 == 0 {
 			d.RegisterHandler(func(i2 int) {
-				fmt.Println("Handled", i2, "in h2")
+				t.Log("Handled", i2, "in h2")
 				if h2 == 0 {
 					d.RegisterHandler(func(i3 int) {
-						fmt.Println("Handled", i3, "in h3")
+						t.Log("Handled", i3, "in h3")
 						h3++
 					})
 				}
@@ -241,9 +254,12 @@ func TestAddHandlerInHandler(t *testing.T) {
 
 // Tests if unregistering handlers works
 func TestUnregisterHandler(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 
 	iInt := 0
+
 	d.RegisterHandler(func(int) {
 		iInt++
 	})
@@ -262,11 +278,13 @@ func TestUnregisterHandler(t *testing.T) {
 	if iInt != 2 {
 		t.Error("The handler that wasn't removed wasn't triggered after removal")
 	}
+
 	if iRemovedInt != 1 {
 		t.Error("The removed handler was triggered after removal")
 	}
 
 	iInterface := 0
+
 	d.RegisterHandler(func(AB) {
 		iInterface++
 	})
@@ -285,6 +303,7 @@ func TestUnregisterHandler(t *testing.T) {
 	if iInterface != 2 {
 		t.Error("The interface handler that wasn't removed wasn't triggered after removal")
 	}
+
 	if iRemovedInterface != 1 {
 		t.Error("The removed interface handler was triggered after removal")
 	}
@@ -292,13 +311,17 @@ func TestUnregisterHandler(t *testing.T) {
 
 // Tests what happens for illegal arguments
 func TestIllegalArguments(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 
 	err := d.RemoveQueues(make(chan interface{}))
 	if err == nil {
 		t.Error("RemoveQueues() returned no error for non-registered queue")
 	}
+
 	err = d.SyncQueues(make(chan interface{}))
+
 	if err == nil {
 		t.Error("SyncQueues() returned no error for non-registered queue")
 	}
@@ -325,12 +348,15 @@ func TestIllegalArguments(t *testing.T) {
 }
 
 func TestConsumerCodePanic(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 	d.RegisterHandler(func(a *A) {
-		fmt.Println(a.val)
+		t.Log(a.val)
 	})
 
 	var err interface{}
+
 	func() {
 		defer func() {
 			err = recover()
@@ -345,6 +371,7 @@ func TestConsumerCodePanic(t *testing.T) {
 		if ccp.String() != "runtime error: invalid memory address or nil pointer dereference" {
 			t.Error("ConsumerCodePanic.String() is not \"runtime error: invalid memory address or nil pointer dereference\"")
 		}
+
 		if fmt.Sprint(ccp.Value()) != "runtime error: invalid memory address or nil pointer dereference" {
 			t.Error("ConsumerCodePanic.Value() is not \"runtime error: invalid memory address or nil pointer dereference\"")
 		}
@@ -355,15 +382,17 @@ func TestConsumerCodePanic(t *testing.T) {
 
 // Just a compile test for the README example
 func TestExample(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 	// Register a handler for string (not *string!)
 	// We get the string Type by calling Elem() on reflect.Type *string)
 	// This is faster than doing reflect.TypeOf("")
 	d.RegisterHandler(func(s string) {
-		fmt.Println("Handled string", s)
+		t.Log("Handled string", s)
 	})
 	d.RegisterHandler(func(obj interface{}) {
-		fmt.Println("Handled object", obj)
+		t.Log("Handled object", obj)
 	})
 
 	d.Dispatch("Hello")
@@ -383,11 +412,13 @@ type TriggerEvent struct{}
 
 // Just a compile test for the README example
 func TestQueueExample(t *testing.T) {
+	t.Parallel()
+
 	d := dp.Dispatcher{}
 	// If you wanted to handle pointers of the Event just remove .Elem(),
 	// use *Event for the type assertion and send pointers
 	d.RegisterHandler(func(e Event) {
-		fmt.Println("Handled Event", e)
+		t.Log("Handled Event", e)
 		// Handle event
 	})
 	d.RegisterHandler(func(te TriggerEvent) {
@@ -412,6 +443,7 @@ func TestQueueExample(t *testing.T) {
 		d.SyncQueues(q)
 		// Do stuff that requires events in q (but not q2 & q3) to be handled
 	}
+
 	d.SyncAllQueues()
 	// Do stuff that requires events of q, q2 & q3 to be handled
 
@@ -429,6 +461,8 @@ func TestQueueExample(t *testing.T) {
 
 // Tests queue functionality
 func TestPanicHandler(t *testing.T) {
+	t.Parallel()
+
 	var recovered interface{}
 
 	d := dp.NewDispatcherWithConfig(dp.Config{
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/markus-wa/godispatch
 
 go 1.11
+
+require github.com/stretchr/testify v1.7.0 // indirect
